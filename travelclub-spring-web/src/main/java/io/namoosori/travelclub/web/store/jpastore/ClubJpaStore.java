bdc20537567d2433bace183a92c249349fbea651
jher235 <tim668666@gmail.com>
2024-01-18 17:06:25 +0900

    spring data jpa를 사용하여 travelclub clud작업 마무리

--- travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/store/jpastore/ClubJpaStore.java
+++ travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/store/jpastore/ClubJpaStore.java
@@ -45,21 +45,22 @@ public class ClubJpaStore implements ClubStore {
 
     @Override
     public List<TravelClub> retrieveByName(String name) {
-        return null;
+        List<TravelClubJpo> clubJpos = clubRepository.findAllByName(name);
+        return clubJpos.stream().map(TravelClubJpo::toDomain).collect(Collectors.toList());
     }
 
     @Override
     public void update(TravelClub club) {
-
+        clubRepository.save(new TravelClubJpo(club));
     }
 
     @Override
     public void delete(String clubId) {
-
+        clubRepository.deleteById(clubId);
     }
 
     @Override
     public boolean exists(String clubId) {
-        return false;
+        return clubRepository.existsById(clubId);
     }
 }
--- travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/store/jpastore/repository/ClubRepository.java
+++ travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/store/jpastore/repository/ClubRepository.java
@@ -4,6 +4,9 @@ import io.namoosori.travelclub.web.aggregate.club.TravelClub;
 import io.namoosori.travelclub.web.store.jpastore.jpo.TravelClubJpo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClubRepository extends JpaRepository<TravelClubJpo, String> {  //JpaRepository를 상속하는 ClubRepository를 정의 후 이를 사용하여 데이터를 저장함
+import java.util.List;
+
+public interface ClubRepository extends JpaRepository<TravelClubJpo, String> {  //JpaRepository를 상속하는 ClubRepository를 정의 후 이를 사용하여 데이터를 저장함 spring data jpa를 사용하기 위함
 //제너릭 첫번째는 엔티티로 정의된 객체, 두번째는 식별자 id임
+    List<TravelClubJpo> findAllByName(String name);//이걸 정의해주면 이 조건에 맞게 쿼리를 만들어서 보내줌
 }
